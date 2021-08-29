f225d49442f7f564f8fa9f7eaccdfb1c33d6fcd9
Jesse Quinn <jesse.quinn@zpesystems.com>
2021-08-29 08:23:41 -0300

    SPA mode false

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import webpack from 'webpack'
 
 export default {
-  ssr: false, // universal mode
+  ssr: true, // universal mode
   server: {
     port: process.env.PORT || 3000,
     host: process.env.HOST || '0.0.0.0',
